1c4f7e1eadfda12bfbfa3fa2374ed9dd2547c2ba
Dominic Myers <annoyingmouse@gmail.com>
2022-04-01 17:39:41 +0100

    All done

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,23 @@
 <script>
   import Counter from "./deep/Counter.svelte";
   import FancyInput from "./FancyInput.svelte";
+  import { numbers } from "./stores";
+  import {fade} from "svelte/transition";
+  // numbers.set(10) - in console
+
   let value = ''
+
+  $: someting = $numbers * 2
 </script>
 
 <FancyInput bind:controlValue={value} label="FRED"/>
 <p>{value}</p>
 
 <Counter count={5}/>
+{#if $numbers > 9}
+  <p transition:fade={{duration:1000}}>{$numbers} {someting}</p>
+{/if}
 
-<p>The text</p>
 
 <style>
   p{
